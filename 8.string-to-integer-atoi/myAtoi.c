022ee89d8b33014a436ce677447270a76e766939
Maboroshi0327 <maboroshi0327@gmail.com>
2026-05-23 16:24:03 +0800

    8. String to Integer (atoi)

--- 8.string-to-integer-atoi/myAtoi.c
+++ 8.string-to-integer-atoi/myAtoi.c
@@ -1,4 +1,30 @@
 #include <stdint.h>
 
 int myAtoi(char *s) {
+    int sign = 1;
+    int result = 0;
+
+    int i = 0;
+    while (s[i] != '\0' && s[i] == ' ')
+        i++;
+
+    switch (s[i]) {
+        case '-':
+            sign = -1;
+        case '+':
+            i++;
+    }
+
+    while (s[i] != '\0' && s[i] >= '0' && s[i] <= '9') {
+        if (result > INT32_MAX / 10 || (result == INT32_MAX / 10 && s[i] > '7')) {
+            result = (sign == 1) ? INT32_MAX : INT32_MIN;
+            return result;
+        }
+        result *= 10;
+        result += s[i] - '0';
+        i++;
+    }
+
+    result *= sign;
+    return result;
 }
