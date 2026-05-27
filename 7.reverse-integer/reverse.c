f830cd6c1678703a5aa7a87695690aa5996a1698
Maboroshi0327 <maboroshi0327@gmail.com>
2026-05-27 16:53:10 +0800

    7. Reverse Integer

---
+++ 7.reverse-integer/reverse.c
@@ -0,0 +1,19 @@
+#include <stdint.h>
+
+int reverse(int x) {
+    int result = 0;
+
+    while (x != 0) {
+        if (result > INT32_MAX / 10 || (result == INT32_MAX / 10 && x % 10 > INT32_MAX % 10))
+            return 0;
+
+        if (result < INT32_MIN / 10 || (result == INT32_MIN / 10 && x % 10 < INT32_MIN % 10))
+            return 0;
+
+        result *= 10;
+        result += x % 10;
+        x /= 10;
+    }
+
+    return result;
+}
