6130cbf7e2fe76e7414f6a6daba29d3ab0e03e39
Maboroshi0327 <maboroshi0327@gmail.com>
2026-05-28 15:16:06 +0800

    27. Remove Element

---
+++ 27.remove-element/removeElement.c
@@ -0,0 +1,12 @@
+int removeElement(int *nums, int numsSize, int val) {
+    int slow = 0;
+
+    for (int fast = 0; fast < numsSize; fast++) {
+        if (nums[fast] != val) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+    }
+
+    return slow;
+}
